716df4fbd8261b116c6779ff8adea28cd29e8c49
shogo.kawakita <higahiga.1920@gmail.com>
2018-06-08 06:25:35 +0000

    Bootstrap introduced

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -3,17 +3,20 @@
 @section('content')
 
     <h1>id: {{ $task->id }}のメッセージ編集ページ</h1>
-
+    <div class="row">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-        
+            <div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
         
         {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+        {!! Form::text('status', null, ['class' => 'form-control']) !!}
     
-        {!! Form::submit('更新') !!}
+        {!! Form::submit('更新', null, ['class' => 'btn btn-default']) !!}
     
     {!! Form::close() !!}
+    </div>
+    </div>
 
 @endsection
